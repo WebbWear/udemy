7b27aaae7fa86fda4838bd6430f37ff0b0a368e0
Jeff Webb <webbwear@icloud.com>
2018-12-12 17:40:58 -0500

    hide the dice

--- complete-javascript-course-master/4-DOM-pig-game/starter/app.js
+++ complete-javascript-course-master/4-DOM-pig-game/starter/app.js
@@ -20,3 +20,8 @@ dice = Math.floor(Math.random() * 6) +1;
 console.log(dice);
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
