507ad1011062be4ab61d0111f4ec7053ee0178e9
Jeff Webb <webbwear@icloud.com>
2018-12-14 10:52:43 -0500

    roll dice complete display to Dom

--- complete-javascript-course-master/4-DOM-pig-game/starter/app.js
+++ complete-javascript-course-master/4-DOM-pig-game/starter/app.js
@@ -9,19 +9,33 @@ GAME RULES:
 
 */
 
-
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) +1;
-console.log(dice);
+document.querySelector(".dice").style.display = "none";
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
 
-document.querySelector('.dice').style.display = 'none';
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    //  random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // display result
+
+    var diceDom = document.querySelector('.dice')
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+   // update round IF user score is not 1
+
+});
+
+
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// var x = document.querySelector("#score-0").textContent;
