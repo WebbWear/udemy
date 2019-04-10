9113ce1bd806fc4ec5c870a2d24902a92319e544
Jeff Webb <webbwear@icloud.com>
2019-04-10 10:47:58 -0400

    Hoisting function/variables

--- complete-javascript-course-master/3-how-JS-works/starter/script.js
+++ complete-javascript-course-master/3-how-JS-works/starter/script.js
@@ -2,15 +2,23 @@
 // Lecture: Hoisting
 
 // functions
-/*
+// function decloration
 calculateAge(1973);
 
 function calculateAge(year) {
-    console.log(2018 - year);
+    console.log(2019 - year);
 }
 
+// function expression ( no hoisting must call function after code)
+var retirement = function(year) {
+    console.log(65 - (2019 - year));
+}
 
-// variables
+retirement(1973);
+
+
+
+// variables Hoisting
 
 var age = 23;
     console.log(age);
@@ -25,7 +33,7 @@ function foo() {
 foo();
 console.log(age);
 
-*/
+
 
 ///////////////////////////////////////
 // Lecture: Scoping
@@ -39,7 +47,7 @@ Each new function creates a scope: the scope/environment, in which the variables
 
 // First scoping example
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -52,18 +60,18 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
+
 
 
 
 // Example to show the differece between execution stack and scope chain
-/*
+
 
 var a = 'Hello!';
 first();
 
 function first() {
-  /*  var b = 'Hi!';
+   var b = 'Hi!';
     second();
 
     function second() {
@@ -74,11 +82,12 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    // console.log(a + b + c + d);
+    console.log(a + d)
 }
 
 
-*/
+
 
 ///////////////////////////////////////
 // Lecture: The this keyword
